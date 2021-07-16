cce5c452dec9258e2a0f4a6ee393fd86067076e9
李凤 <459559238>
2021-07-16 21:50:26 +0800

    go-1.16.15-https certification convert

--- certification_convert.go
+++ certification_convert.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// stack: https://stackoverflow.com/questions/12122159/how-to-do-a-https-request-with-bad-certificate
 // https://github.com/golang/go/issues/47143
 // crypto/tls: clients can panic when provided a certificate of the wrong type for the negotiated parameters (CVE-2021-34558)
 func main() {
 	// todo: use bad certification to connect https website
-	_, err := http.Get("https://gocn.vip/")
+	caCert, err := ioutil.ReadFile("golang.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs: caCertPool,
+				//InsecureSkipVerify: true,
+			},
+		},
+	}
+
+	_, err = client.Get("https://gocn.vip/")
 	if err != nil {
 		log.Fatal(err)
 	}
